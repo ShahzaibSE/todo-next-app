2ba3c4607ddbb57638eeff7522f9b95c7adb6c8b
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-01-16 23:52:35 +0500

    [FEAT] - Setting up component for displaying tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+"use client";
+
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-// Chakra Modules
-import { Heading } from '@chakra-ui/react'
 import styles from "./page.module.css";
 import {
   Table,
@@ -13,6 +13,16 @@ import {
   Td,
   TableCaption,
   TableContainer,
+  Divider,
+  Heading,
+  Box,
+  Card,
+  CardHeader,
+  CardBody,
+  Flex,
+  Spacer,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,45 +30,54 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div style={{ margin: "0 auto" }}>
-        <h1>To-Do</h1>
-        <TableContainer>
-          <Table variant="simple">
-            <TableCaption>Imperial to metric conversion factors</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th isNumeric>multiply by</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-                <Td isNumeric>25.4</Td>
-              </Tr>
-              <Tr>
-                <Td>feet</Td>
-                <Td>centimetres (cm)</Td>
-                <Td isNumeric>30.48</Td>
-              </Tr>
-              <Tr>
-                <Td>yards</Td>
-                <Td>metres (m)</Td>
-                <Td isNumeric>0.91444</Td>
-              </Tr>
-            </Tbody>
-            <Tfoot>
-              <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th isNumeric>multiply by</Th>
-              </Tr>
-            </Tfoot>
-          </Table>
-        </TableContainer>
-      </div>
+      <Card>
+        <CardHeader>
+          <Heading as="h2" size="xl">
+            Tasks
+          </Heading>
+        </CardHeader>
+        <Divider orientation="horizontal" />
+        <CardBody>
+          <Box>
+            <TableContainer>
+              <Table variant="simple">
+                <TableCaption>These are your tasks</TableCaption>
+                <Thead>
+                  <Tr>
+                    <Th>To convert</Th>
+                    <Th>into</Th>
+                    <Th isNumeric>multiply by</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>inches</Td>
+                    <Td>millimetres (mm)</Td>
+                    <Td isNumeric>25.4</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>feet</Td>
+                    <Td>centimetres (cm)</Td>
+                    <Td isNumeric>30.48</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>yards</Td>
+                    <Td>metres (m)</Td>
+                    <Td isNumeric>0.91444</Td>
+                  </Tr>
+                </Tbody>
+                <Tfoot>
+                  <Tr>
+                    <Th>To convert</Th>
+                    <Th>into</Th>
+                    <Th isNumeric>multiply by</Th>
+                  </Tr>
+                </Tfoot>
+              </Table>
+            </TableContainer>
+          </Box>
+        </CardBody>
+      </Card>
     </main>
   );
 }
