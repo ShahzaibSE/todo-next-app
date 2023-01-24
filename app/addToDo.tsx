95992f3521cb87deca0c5e52e19551256749459c
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-01-25 00:29:55 +0500

    FEAT: Integerating mock APIS

--- app/addToDo.tsx
+++ app/addToDo.tsx
@@ -20,10 +20,20 @@ import {
   Input,
   IconButton,
   HStack,
+  UseRadioGroupReturn,
 } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import axios from "axios";
+// import { add_to_do } from "./todo.controller";
+
+export async function add_to_do(name: string, refresh: any) {
+  await axios.post(`http://localhost:3001/api/todo/add`);
+  refresh();
+}
 
 export default function AddToDo() {
+  const router = useRouter();
+  const [name, setName] = useState("");
   return (
     <HStack>
       <Box maxWidth="100%">
@@ -36,6 +46,10 @@ export default function AddToDo() {
           aria-label="Add Task"
           icon={<AddIcon />}
           colorScheme="green"
+          // onClick={async () => {
+          //   await add_to_do(name, router.reload);
+          //   setName("");
+          // }}
         />
       </Box>
     </HStack>
--- app/todo.controller.ts
+++ app/todo.controller.ts
@@ -6,3 +6,8 @@ export async function getToDos() {
     todo = await axios.get("http://localhost:3001/api/todo/list");
     return todos;
 }
+
+export async function add_to_do(name: string, refresh:any){
+    await axios.post(`http://localhost:3001/api/todo/add`);
+    refresh();
+  }
